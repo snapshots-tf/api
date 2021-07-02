5c6fc6e88acb1a418ec95667b8033ffd52e7986c
Viggo Jonasson <aethezjonasson@gmail.com>
2021-07-03 00:17:28 +0200

    fix: sorting issues (https://stackoverflow.com/questions/22797768/does-mongodbs-in-clause-guarantee-order)

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -20,19 +20,23 @@ export async function returnListings(
 ): Promise<SnapshotNamespace.Listing[]> {
     // @ts-ignore
     return model.find({ _id: { $in: ids } }).then((res) =>
-        res.map((listing) => {
-            const obj = Object.assign(
-                {
-                    id: listing._id,
-                    savedAt: listing.savedAt,
-                    lastSeen: listing.lastSeen,
-                },
-                listing.listing
-            );
+        res
+            .sort((a, b) => {
+                return ids.indexOf(a._id) - ids.indexOf(b._id);
+            })
+            .map((listing) => {
+                const obj = Object.assign(
+                    {
+                        id: listing._id,
+                        savedAt: listing.savedAt,
+                        lastSeen: listing.lastSeen,
+                    },
+                    listing.listing
+                );
 
-            // @ts-ignore
-            delete obj.$init;
-            return obj;
-        })
+                // @ts-ignore
+                delete obj.$init;
+                return obj;
+            })
     );
 }
