414f23a953e935e1943be81c134ccdc005d9476e
Viggo Jonasson <aethezjonasson@gmail.com>
2021-07-13 16:33:47 +0200

    fix: make keyprice retry

--- src/snapshotting/keyprices.service.ts
+++ src/snapshotting/keyprices.service.ts
@@ -1,6 +1,7 @@
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable, Logger } from '@nestjs/common';
 import axios from 'axios';
+import { promiseDelay } from 'src/lib/helpers';
 import * as Currencies from 'tf2-currencies-lite';
 
 export interface IGetCurrenciesResponse {
@@ -27,20 +28,30 @@ export class KeyPricesService {
     }
 
     async check(): Promise<void> {
-        const { data } = (await axios({
-            method: 'GET',
-            url: 'https://backpack.tf/api/IGetCurrencies/v1',
-            params: {
-                key: process.env.BPTF_API_KEY,
-            },
-            timeout: 10 * 1000,
-        })) as { data: IGetCurrenciesResponse };
+        try {
+            const { data } = (await axios({
+                method: 'GET',
+                url: 'https://backpack.tf/api/IGetCurrencies/v1',
+                params: {
+                    key: process.env.BPTF_API_KEY,
+                },
+                timeout: 10 * 1000,
+            })) as { data: IGetCurrenciesResponse };
 
-        keyPrice = new Currencies({
-            metal: data.response.currencies.keys.price.value,
-            keys: 0,
-        });
+            keyPrice = new Currencies({
+                metal: data.response.currencies.keys.price.value,
+                keys: 0,
+            });
 
-        this.logger.debug('Got new keyprice!', keyPrice);
+            this.logger.debug('Got new keyprice!', keyPrice);
+        } catch (err) {
+            this.logger.warn(
+                'Failed to get keyprice, trying again in 10 seconds!'
+            );
+
+            return promiseDelay(10 * 1000).then(() => {
+                return this.check();
+            });
+        }
     }
 }
