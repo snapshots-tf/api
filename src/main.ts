3f3d02201505456381172b618c877044de20d309
Viggo Jonasson <aethezjonasson@gmail.com>
2021-07-16 14:56:56 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://localhost:3000', 'https://snapshots.tf'],
+        origin: [
+            'http://localhost:3000',
+            'https://snapshots.tf',
+            'https://app.snapshots.tf/',
+        ],
         credentials: true,
     });
 
