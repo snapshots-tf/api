5d6fd08b2cb720976a98f186ffef53fece95b6af
Viggo Jonasson <aethezjonasson@gmail.com>
2021-07-16 15:00:58 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,12 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: [
+        origin: true,
+        /*[
             'http://localhost:3000',
             'https://snapshots.tf',
             'https://app.snapshots.tf/',
-        ],
+        ],*/
         credentials: true,
     });
 
