dd1f15049a2d05c55da9bce4e1a5ca0ab77a435d
Viggo Jonasson <aethezjonasson@gmail.com>
2021-07-24 12:22:55 +0200

    origin as star?

--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: [
-            'http://localhost:3000',
-            'https://snapshots.tf',
-            'https://app.snapshots.tf/',
-        ],
+        origin: '*',
         methods: ['GET', 'PUT', 'POST'],
         credentials: true,
     });
